e856de5d00286cbdb69bee0ab899a08aa35df997
Thomas Asnong <r0839692@ucll.be>
2026-07-20 18:25:00 +0200

    Integratietest voor campus-api toegevoegd

---
+++ campus-api/src/test/resources/test.sql
@@ -0,0 +1,8 @@
+INSERT INTO campus (name, address, number_of_parking_spaces)
+VALUES ('TestCampus1', 'TestAddress1', 10);
+
+INSERT INTO campus (name, address, number_of_parking_spaces)
+VALUES ('TestCampus2', 'TestAddress2', 20);
+
+INSERT INTO campus (name, address, number_of_parking_spaces)
+VALUES ('TestCampus3', 'TestAddress3', 30);
