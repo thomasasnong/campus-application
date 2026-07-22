79dd1b3ef6fc546617a5e3bde54ced9299c4b28b
Thomas Asnong <r0839692@ucll.be>
2026-07-22 13:43:16 +0200

    Usercontrollerlaag + testdata toegevoegd

--- campus-api/src/main/resources/data.sql
+++ campus-api/src/main/resources/data.sql
@@ -16,3 +16,12 @@ VALUES ('TestRoomSameName', 'TestType2', 20, 2, 'TestCampus1');
 
 INSERT INTO room (name, room_type, number_of_seats, floor, campus_name)
 VALUES ('TestRoomSameName', 'TestType1', 30, 1, 'TestCampus2');
+
+INSERT INTO app_user (last_name, first_name, birth_date, email)
+VALUES ('TestLastName1', 'TestFirstName1', '2001-01-01', 'testuser1@email.com');
+
+INSERT INTO app_user (last_name, first_name, birth_date, email)
+VALUES ('TestLastName2', 'TestFirstName2', '2002-02-02', 'testuser2@email.com');
+
+INSERT INTO app_user (last_name, first_name, birth_date, email)
+VALUES ('Doe', 'John', '2003-03-03', 'john.doe@email.com');
