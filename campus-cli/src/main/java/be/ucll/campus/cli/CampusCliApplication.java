d44cee85cd8908fbf1b1a04348431421a378d760
Thomas Asnong <r0839692@ucll.be>
2026-07-25 14:13:25 +0200

    Hoofdmenu met invoercontrole toegevoegd

--- campus-cli/src/main/java/be/ucll/campus/cli/CampusCliApplication.java
+++ campus-cli/src/main/java/be/ucll/campus/cli/CampusCliApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class CampusCliApplication implements CommandLineRunner {
 
     private final CampusApiClient campusApiClient;
+    private final Scanner scanner = new Scanner(System.in);
 
     public CampusCliApplication(CampusApiClient campusApiClient) {
         this.campusApiClient = campusApiClient;
@@ -23,12 +25,42 @@ public class CampusCliApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        List<Campus> campuses = campusApiClient.getCampuses();
+        boolean runApplication = true;
 
-        System.out.println("Campussen:");
+        while (runApplication) {
+            System.out.println("CAMPUS COMMANDLINE-APP");
+            System.out.println("----------------------");
+            System.out.println("1. Gebruiker aanmaken");
+            System.out.println("2. Reservatie maken");
+            System.out.println("0. Afsluiten");
+            System.out.println("----------------------");
 
-        for (Campus campus : campuses) {
-            System.out.println("- " + campus.getName());
+            int choice;
+
+            while (true) {
+                System.out.print("Keuze: ");
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                    break;
+                } catch (NumberFormatException exception) {
+                    System.out.println("Ongeldige invoer, kies een nummer uit de keuzelijst.");
+                }
+            }
+
+            switch (choice) {
+                case 1 ->
+                    System.out.println("Gebruiker aanmaken");
+                case 2 ->
+                    System.out.println("Reservatie maken");
+                case 0 ->
+                    runApplication = false;
+                default ->
+                    System.out.println("Ongeldige keuze.");
+            }
+
+            System.out.println();
         }
+
+        System.out.println("Applicatie afgesloten");
     }
 }
